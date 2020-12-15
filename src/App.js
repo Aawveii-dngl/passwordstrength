735c435285d5fcdbf22a47118ce08f23da0de8c8
Avi Dangol <dangolavi9999@gmail.com>
2020-12-15 16:10:51 +0545

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Container, Form, Button, Card, Jumbotron, ProgressBar } from 'react-bootstrap';
+import Strengthmeter from './Strengthmeter';
+import {useState} from 'react';
 
 function App() {
+  const[password,setPassword] = useState('');
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Jumbotron className="m-3">
+        <h3 className="text-center">Password Strength Checker</h3>
+      </Jumbotron>
+        <Card className="m-5 p-5">
+          <h4 className="text-center"><b>Registration Form</b></h4>
+          <br></br>
+          <Form>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control 
+                type="email" 
+                placeholder="Enter email" 
+              />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
+
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control 
+                type="password"  
+                placeholder="Password" 
+                onChange = {(e) =>setPassword(e.target.value)}
+              />
+            </Form.Group>
+            <Strengthmeter password = {password}/>
+
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>ConfirmPassword</Form.Label>
+              <Form.Control 
+                type="password"  
+                placeholder="Password" 
+              />
+            </Form.Group>
+
+            <Form.Group controlId="formBasicCheckbox">
+              <Form.Check 
+                type="checkbox" 
+                label="Check me out" 
+              />
+            </Form.Group>
+
+
+            <Button 
+              variant="primary"
+              type="submit">
+              Submit
+            </Button>
+            
+          </Form>
+          </Card>
+    </Container>
   );
 }
 
---
+++ src/Strengthmeter.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import {ProgressBar} from 'react-bootstrap'
+import zxcvbn from 'zxcvbn';
+
+export default function Strengthmeter({password}) {
+    const Testresult = zxcvbn(password);
+    const num = Testresult.score* 100/4;
+
+    const Createpwlabel = () =>{
+        switch(Testresult.score){
+            case 0:
+                return 'Very weak';
+            case 1:
+                return 'Weak';
+            case 2:
+                return 'Fair';
+            case 3:
+                return 'Good';
+            case 4:
+                return 'Strong';
+            default:
+                return 'none';
+        }
+    }
+
+    const funcnProgress = () =>{
+        switch(Testresult.score){
+            case 0:
+                return 'white';
+            case 1:
+                return 'blue';
+            case 2:
+                return 'green';
+            case 3:
+                return 'orange';
+            case 4:
+                return 'red';
+            default:
+                return 'none';
+        }
+    }
+
+    const changeProclcr = () =>({
+        width: `${num}%`,
+        background:funcnProgress(),
+
+    })
+    return (
+       <div>
+        <div className = 'progress' style={{height:'px'}}>
+                <div className = 'progress-bar' style = {changeProclcr()}></div>
+        </div>
+        <p style ={{color:funcnProgress()}} class="text-right">{Createpwlabel()}</p>
+       </div>
+    )
+}
